b8c24e48e0b33b6424b97f1124c2e99e7b6606db
pasheva <pashevam@outlook.com>
2020-11-25 23:24:34 -0600

    orgnz

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
-# CostConstrainedShortestPath
+# CostConstrainedShortestPath Algo NP-Hard
+Compile
+``` c++
+./src
+make clean
+make cpath
+```
+Run
+```
+./cpath <file> <s> <d> <budget>
+```
---
+++ src/cpath.cpp
@@ -0,0 +1,38 @@
+#include<iostream>
+#include "CPath.h"
+
+using namespace std;
+
+int main(int argc, char *argv[]){
+
+
+    graph g;
+    if(argc != 5) {
+        std::cout << "usage:  ./cpath <file> <s> <d> <budget>\n";
+        return 0;
+    }else {
+        string path = "./../graphs/";
+        path += argv[1];
+        if(!g.read_file(path)){ // Reading file
+            std::cout << "could not open file '" << argv[1] << "'\n";
+            return 0;
+        }
+    }
+
+    double budget = stoi(argv[4]);
+    string src = argv[2];
+    string dst = argv[3];
+
+    CPath<double,double,string> myPath(g,budget);
+
+    myPath.findPaths(src);
+    // PARETO TRADEOFF CURVE | NON-DOMINATED PATHS
+    myPath.checkP(dst);
+    // COST-FEASIBLE AND FASTEST PAIR(COST, TIME) && ACTUAL PATH
+    myPath.feasiblePath(src,dst);
+
+
+    cout << endl;
+    return 0;
+}
+
